d6af9ffbbfd55cd263dc33b7d905ac7016118fde
AkhmatovSardor <sardor.akhmatov0969@gmail.com>
2023-08-28 14:48:49 +0500

    first commit

---
+++ src/main/java/com/company/Bankschemagetaway/BankSchemaGetawayApplication.java
@@ -0,0 +1,13 @@
+package com.company.Bankschemagetaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSchemaGetawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSchemaGetawayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Bankschemagetaway/BankSchemaGetawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Bankschemagetaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSchemaGetawayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
